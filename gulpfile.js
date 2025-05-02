c5770aea27b55b3c15b3c33d89479d7074841c55
Diego <diegosirtoli@hotmail.com>
2025-05-02 17:14:31 -0300

    setup do projeto 1 - agora minificado

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const cleanCss = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
+const paths = {
+    scss: './src/styles/*.scss',
+    css: './dist/css/'
+}
 
 function styles() {
-    return gulp.src('./src/styles/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+    return gulp.src(paths.scss)
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError)) // Minifica o CSS durante a compilação
+    .pipe(cleanCss()) // Aplica uma minificação extra
+    .pipe(rename({ suffix: '.min' })) // Renomeia o arquivo para main.min.css
+    .pipe(gulp.dest(paths.css));
+}
+
+function watchFiles() {
+    gulp.watch(paths.scss, gulp.series(styles)); 
 }
 
 exports.default = styles;
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-}
+exports.watch = watchFiles;
