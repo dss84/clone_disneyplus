cd509bd2f12450944a443f7756e8520c0ac4be6c
Diego <diegosirtoli@hotmail.com>
2025-05-26 16:25:06 -0300

    criando o hero e impordo a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const cleanCss = require('gulp-clean-css');
-const rename = require('gulp-rename');
-
-const paths = {
-    scss: './src/styles/*.scss',
-    css: './dist/css/'
-}
+const imagemin = require('gulp-imagemin');
 
 function styles() {
-    return gulp.src(paths.scss)
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError)) 
-    .pipe(cleanCss()) 
-    .pipe(rename({ suffix: '.min' })) 
-    .pipe(gulp.dest(paths.css));
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dist/css'));
 }
 
-function watchFiles() {
-    gulp.watch(paths.scss, gulp.series(styles)); 
+function images() {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = styles;
-exports.watch = watchFiles;
+exports.default = gulp.parallel(styles, images);
+
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
